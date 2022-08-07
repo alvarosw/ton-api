13c0d54651feb110c89d709a7b5e4e4aa9a391fe
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-07 18:43:23 -0300

    feat: first commit - hello world project setup

---
+++ src/hello-world/handlers.ts
@@ -0,0 +1,30 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    let response: APIGatewayProxyResult;
+    try {
+        response = {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        response = {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+
+    return response;
+};
