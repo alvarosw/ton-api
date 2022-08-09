93f24097012b45c950f6eb3fdb4c2915d9cd9a8c
Álvaro <56317536+alvarosw@users.noreply.github.com>
2022-08-09 01:39:56 -0300

    refactor: padronize responses

--- src/users/handlers.ts
+++ src/users/handlers.ts
@@ -1,7 +1,7 @@
-import { GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { badResponse, response } from '../utils';
+import { GetItemCommand, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { response } from '../utils';
 import db from '../db';
 
 const DB_PARAMS = { TableName: process.env.USERS_TABLE };
@@ -11,17 +11,9 @@ export async function getUser(event: APIGatewayProxyEvent): Promise<APIGatewayPr
     const params = { ...DB_PARAMS, Key: marshall({ userId: event.pathParameters.userId }) };
     const { Item } = await db.send(new GetItemCommand(params));
 
-    return response(200, {
-      message: 'Successfully retrieved user.',
-      data: Item ? unmarshall(Item) : {},
-      rawData: Item,
-    });
+    return response(200, unmarshall(Item || {}));
   } catch (e) {
-    return response(500, {
-      message: 'Failed to get user.',
-      errorMsg: (<any>e).message,
-      errorStack: (<any>e).stack,
-    });
+    return badResponse(500, 'Failed to get user');
   }
 }
 
@@ -31,15 +23,8 @@ export async function createUser(event: APIGatewayProxyEvent): Promise<APIGatewa
     const params = { ...DB_PARAMS, Item: marshall(data || {}) };
     const createResult = await db.send(new PutItemCommand(params));
 
-    return response(200, {
-      message: 'Successfully created user.',
-      createResult,
-    });
+    return response(200, createResult);
   } catch (e) {
-    return response(500, {
-      message: 'Failed to create user.',
-      errorMsg: (<any>e).message,
-      errorStack: (<any>e).stack,
-    });
+    return badResponse(500, 'Failed to create user');
   }
 }
